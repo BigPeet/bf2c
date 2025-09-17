94d38d5f140fac89b176dc2d7b39d9f21ba47579
Peter Wolf <pwolf2310@gmail.com>
2025-09-17 22:23:38 +0200

    add CMake options, sanitizers

---
+++ ProjectOptions.cmake
@@ -0,0 +1,47 @@
+macro(setup_options)
+
+  # Dev Mode
+  option(DEV_MODE "Enable developer mode" OFF)
+  if(${DEV_MODE})
+    option(ENABLE_SANITIZERS "Enable sanitizers" ON)
+    option(ENABLE_TESTS "Enable tests" ON)
+    if (NOT CMAKE_BUILD_TYPE)
+      set(CMAKE_BUILD_TYPE "Debug")
+    endif()
+  else()
+    option(ENABLE_SANITIZERS "Enable sanitizers" OFF)
+    option(ENABLE_TESTS "Enable tests" OFF)
+    if (NOT CMAKE_BUILD_TYPE)
+      if (NOT ${DEFAULT_BUILD_TYPE})
+        set(CMAKE_BUILD_TYPE "Release")
+      else()
+        set(CMAKE_BUILD_TYPE ${DEFAULT_BUILD_TYPE})
+    endif()
+  endif()
+
+  # Determine if this is a release build
+  if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
+    set(IS_RELEASE_BUILD 1)
+  else()
+    set(IS_RELEASE_BUILD 0)
+  endif()
+
+  # If build type is a release build, then disable logging prefix by default.
+  if(${IS_RELEASE_BUILD})
+    option(LOGGING_NO_PREFIX "Log without file prefix" OFF)
+  else()
+    option(LOGGING_NO_PREFIX "Log without file prefix" ON)
+  endif()
+
+  # Sanitizers
+  if(MSVC)
+    option(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR "Enable undefined sanitizer" OFF)
+  else()
+    option(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR "Enable undefined sanitizer" ON)
+  endif()
+  option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ON)
+  option(ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+  option(ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+  option(ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+
+endmacro()
